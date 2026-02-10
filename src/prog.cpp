0be7772b651d6eeeb5b82ae991e751c24869a0d1
ardakk-gamer <131574903+ardakk-gamer@users.noreply.github.com>
2026-02-10 21:26:26 +0300

    Create prog.cpp

---
+++ src/prog.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string gameName = "EchoSpark Horror Project";
+    std::cout << "Starting build process for: " << gameName << std::endl;
+
+    std::cout << "Build successful!" << std::endl;
+    return 0;
+}

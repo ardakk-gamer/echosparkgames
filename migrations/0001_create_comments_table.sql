6fbfc1859d3e818015863ba65f264c2b4f15bfe7
ardakk-gamer <131574903+ardakk-gamer@users.noreply.github.com>
2026-02-10 21:28:32 +0300

    Update 0001_create_comments_table.sql

--- migrations/0001_create_comments_table.sql
+++ migrations/0001_create_comments_table.sql
@@ -1,14 +1,5 @@
--- Migration number: 0001 	 2024-12-27T22:04:18.794Z
-CREATE TABLE IF NOT EXISTS comments (
-    id INTEGER PRIMARY KEY NOT NULL,
-    author TEXT NOT NULL,
-    content TEXT NOT NULL
+CREATE TABLE tasks (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  content TEXT,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
-
--- Insert some sample data into our comments table.
-INSERT INTO comments (author, content)
-VALUES
-    ('Kristian', 'Congrats!'),
-    ('Serena', 'Great job!'),
-    ('Max', 'Keep up the good work!')
-;
